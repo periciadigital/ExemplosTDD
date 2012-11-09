07d4d8f57b5d9a04ac880c4f54941247fc11ca94
Rodolfo Mendes <rodolfo.m.s.mendes@gmail.com>
2012-11-08 22:57:16 -0200

    ordenacao: aplicando solucao anterior para arryas.length > 2

--- test/ordenacao/SorterTest.java
+++ test/ordenacao/SorterTest.java
@@ -5,24 +5,32 @@ import org.junit.Test;
 
 /**
  * @author Rodolfo Mendes
- *
+ * 
  */
 public class SorterTest {
 	private final Sorter sorter = new Sorter();
 
 	@Test
-	public void arrayComUmElementoJaEstaOrdenado(){
-		int[] arrayToBeOrdered = {10};
+	public void arrayComUmElementoJaEstaOrdenado() {
+		int[] arrayToBeOrdered = { 10 };
 		int[] arrayOrdenado = sorter.sort(arrayToBeOrdered);
-		
+
 		Assert.assertArrayEquals(arrayToBeOrdered, arrayOrdenado);
 	}
-	
+
 	@Test
-	public void arrayComDoisElementosTrocados(){
-		int[] arrayToBeOrdered = {20,10};
+	public void arrayComDoisElementosTrocados() {
+		int[] arrayToBeOrdered = { 20, 10 };
 		int[] arrayOrdenado = sorter.sort(arrayToBeOrdered);
-		
-		Assert.assertArrayEquals(new int[]{10, 20}, arrayOrdenado);
+
+		Assert.assertArrayEquals(new int[] { 10, 20 }, arrayOrdenado);
+	}
+
+	@Test
+	public void ordenarArrayPermutandoPares() {
+		int[] arrayToBeOrdered = { 20, 10, 40, 30, 60, 50 };
+		int[] arrayOrdenado = sorter.sort(arrayToBeOrdered);
+
+		Assert.assertArrayEquals(new int[] { 10, 20, 30, 40, 50, 60 }, arrayOrdenado);
 	}
 }
