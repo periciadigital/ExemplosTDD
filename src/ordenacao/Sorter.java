e2847fbb51cb9e22b7c0b57236b3ee2cdcb0b70f
Rodolfo Mendes <rodolfo.m.s.mendes@gmail.com>
2012-11-08 22:37:39 -0200

    ordenacao: generalizando 2o teste (4)

--- src/ordenacao/Sorter.java
+++ src/ordenacao/Sorter.java
@@ -2,8 +2,7 @@ package ordenacao;
 
 public class Sorter {
 	public int[] sort(int[] arrayToBeOrdered) {
-		if(arrayToBeOrdered.length == 1){
-		}else{
+		if(arrayToBeOrdered.length > 1){
 			int aux = arrayToBeOrdered[0];
 			arrayToBeOrdered[0] = arrayToBeOrdered[1];
 			arrayToBeOrdered[1] = aux;
