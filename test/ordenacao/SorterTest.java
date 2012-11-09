0108ccf3129f93d49b35ccd31cb33c1a1d84a528
Rodolfo Mendes <rodolfo.m.s.mendes@gmail.com>
2012-11-08 22:01:41 -0200

    ordenacao: caso mais simples possivel

---
+++ test/ordenacao/SorterTest.java
@@ -0,0 +1,14 @@
+package ordenacao;
+
+import org.junit.Test;
+
+/**
+ * @author Rodolfo Mendes
+ *
+ */
+public class SorterTest {
+	@Test
+	public void arrayComUmElementoJaEstaOrdenado(){
+		
+	}
+}
