1bb3afcb80190bf20dcf050390cc3849d4684099
Rodolfo Mendes <rodolfo.m.s.mendes@gmail.com>
2012-11-08 22:33:59 -0200

    ordenacao: generalizando 2o teste (1)

--- src/ordenacao/Sorter.java
+++ src/ordenacao/Sorter.java
@@ -6,6 +6,10 @@ public class Sorter {
 			return arrayToBeOrdered;
 		}
 		
-		return new int[]{10,20};
+		int aux = arrayToBeOrdered[0];
+		arrayToBeOrdered[0] = arrayToBeOrdered[1];
+		arrayToBeOrdered[1] = aux;
+		
+		return arrayToBeOrdered;
 	}
 }
