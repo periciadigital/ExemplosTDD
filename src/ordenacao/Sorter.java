0326f22702714fd29365bfdfa33d3541c3543828
Rodolfo Mendes <rodolfo.m.s.mendes@gmail.com>
2012-11-08 23:04:07 -0200

    ordenacao: fazendo teste 3 passar

--- src/ordenacao/Sorter.java
+++ src/ordenacao/Sorter.java
@@ -2,7 +2,19 @@ package ordenacao;
 
 public class Sorter {
 	public int[] sort(int[] arrayToBeOrdered) {
-		if(arrayToBeOrdered.length > 1){
+		if(arrayToBeOrdered.length > 2){
+			int aux = arrayToBeOrdered[0];
+			arrayToBeOrdered[0] = arrayToBeOrdered[1];
+			arrayToBeOrdered[1] = aux;
+			
+			aux = arrayToBeOrdered[2];
+			arrayToBeOrdered[2] = arrayToBeOrdered[3];
+			arrayToBeOrdered[3] = aux;
+			
+			aux = arrayToBeOrdered[4];
+			arrayToBeOrdered[4] = arrayToBeOrdered[5];
+			arrayToBeOrdered[5] = aux;
+		}else if(arrayToBeOrdered.length > 1){
 			int aux = arrayToBeOrdered[0];
 			arrayToBeOrdered[0] = arrayToBeOrdered[1];
 			arrayToBeOrdered[1] = aux;
