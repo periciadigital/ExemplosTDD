9a04906d8cb7af2a04ac04ccd603448085152f4e
Rodolfo Mendes <rodolfo.m.s.mendes@gmail.com>
2012-11-08 22:15:07 -0200

    ordenacao: teste falhando

---
+++ src/ordenacao/Sorter.java
@@ -0,0 +1,7 @@
+package ordenacao;
+
+public class Sorter {
+	public int[] sort(int[] arrayToBeOrdered) {
+		return null;
+	}
+}
--- test/ordenacao/SorterTest.java
+++ test/ordenacao/SorterTest.java
@@ -10,6 +10,8 @@ import org.junit.Test;
 public class SorterTest {
 	@Test
 	public void arrayComUmElementoJaEstaOrdenado(){
+		Sorter sorter = new Sorter();
+		
 		int[] arrayOrdenado = sorter.sort(new int[]{10});
 		
 		Assert.assertArrayEquals(new int[]{10}, arrayOrdenado);
