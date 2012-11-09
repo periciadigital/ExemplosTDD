b4ccce6370ec10150153b5f39eeefcc9d272ff6c
Rodolfo Mendes <rodolfo.m.s.mendes@gmail.com>
2012-11-08 23:39:24 -0200

    ordenacao: regredindo o teste

--- test/ordenacao/SorterTest.java
+++ test/ordenacao/SorterTest.java
@@ -41,12 +41,12 @@ public class SorterTest {
 
 		Assert.assertArrayEquals(new int[] { 10, 20, 30, 40, 50, 60 }, arrayOrdenado);
 	}
-	
-	@Test(timeout=5000)
-	public void ordenarArrayInvertido(){
-		int[] arrayToBeOrdered = { 50, 40, 30, 20, 10 };
+
+	@Test
+	public void permutandoParesComprimentoImparUltimoNaoEhMaior(){
+		int[] arrayToBeOrdered = { 20, 10, 40, 30, 15};
 		int[] arrayOrdenado = sorter.sort(arrayToBeOrdered);
 
-		Assert.assertArrayEquals(new int[] { 10, 20, 30, 40, 50}, arrayOrdenado);
+		Assert.assertArrayEquals(new int[] { 10, 15, 20, 30, 40}, arrayOrdenado);
 	}
 }
