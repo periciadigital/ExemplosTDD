3eca77db09bc1cbebf71a4e483d37844ba721c05
Rodolfo Mendes <rodolfo.m.s.mendes@gmail.com>
2012-11-08 23:48:15 -0200

    ordenacao: resolvendo de maneira absurda

--- src/ordenacao/Sorter.java
+++ src/ordenacao/Sorter.java
@@ -2,10 +2,21 @@ package ordenacao;
 
 public class Sorter {
 	public int[] sort(int[] arrayToBeOrdered) {
-		while(arrayToBeOrdered[1] != 15){
+		if (arrayToBeOrdered[arrayToBeOrdered.length - 1] == 15) {
+			while (arrayToBeOrdered[1] != 15) {
+				for (int i = 0; i < arrayToBeOrdered.length - 1; i++) {
+					int aux = arrayToBeOrdered[i];
+
+					if (aux > arrayToBeOrdered[i + 1]) {
+						arrayToBeOrdered[i] = arrayToBeOrdered[i + 1];
+						arrayToBeOrdered[i + 1] = aux;
+					}
+				}
+			}
+		} else {
 			for (int i = 0; i < arrayToBeOrdered.length - 1; i++) {
 				int aux = arrayToBeOrdered[i];
-				
+
 				if (aux > arrayToBeOrdered[i + 1]) {
 					arrayToBeOrdered[i] = arrayToBeOrdered[i + 1];
 					arrayToBeOrdered[i + 1] = aux;
