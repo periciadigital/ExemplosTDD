1e3a7342a70d0a81c52b832bc02c51fd692b26e9
Rodolfo Mendes <rodolfo.m.s.mendes@gmail.com>
2012-11-08 23:58:56 -0200

    ordenacao: versao final

--- src/ordenacao/Sorter.java
+++ src/ordenacao/Sorter.java
@@ -2,29 +2,22 @@ package ordenacao;
 
 public class Sorter {
 	public int[] sort(int[] arrayToBeOrdered) {
-		while (!isOrdered(arrayToBeOrdered)) {
+		int trocas = Integer.MAX_VALUE;
+
+		while (trocas > 0) {
+			trocas = 0;
+
 			for (int i = 0; i < arrayToBeOrdered.length - 1; i++) {
 				int aux = arrayToBeOrdered[i];
 
 				if (aux > arrayToBeOrdered[i + 1]) {
 					arrayToBeOrdered[i] = arrayToBeOrdered[i + 1];
 					arrayToBeOrdered[i + 1] = aux;
+					trocas++;
 				}
 			}
 		}
 
 		return arrayToBeOrdered;
-	}
-
-	private boolean isOrdered(int[] arrayToBeOrdered) {
-		for (int i = 0; i < arrayToBeOrdered.length - 1; i++) {
-			int aux = arrayToBeOrdered[i];
-
-			if (aux > arrayToBeOrdered[i + 1]) {
-				return false;
-			}
-		}
-
-		return true;
 	}
 }
