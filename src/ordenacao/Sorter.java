38f0e34907b77c87305da9f85aac9fc26fd5e0ea
Rodolfo Mendes <rodolfo.m.s.mendes@gmail.com>
2012-11-08 23:11:28 -0200

    ordenacao: refatorando 3 teste (2)

--- src/ordenacao/Sorter.java
+++ src/ordenacao/Sorter.java
@@ -3,25 +3,19 @@ package ordenacao;
 public class Sorter {
 	public int[] sort(int[] arrayToBeOrdered) {
 		int i = 0;
-		
-		if(arrayToBeOrdered.length > 2){
-			int aux = arrayToBeOrdered[i];
-			arrayToBeOrdered[i] = arrayToBeOrdered[i + 1];
-			arrayToBeOrdered[i + 1] = aux;
-			
-			aux = arrayToBeOrdered[2];
-			arrayToBeOrdered[2] = arrayToBeOrdered[3];
-			arrayToBeOrdered[3] = aux;
-			
-			aux = arrayToBeOrdered[4];
-			arrayToBeOrdered[4] = arrayToBeOrdered[5];
-			arrayToBeOrdered[5] = aux;
-		}else if(arrayToBeOrdered.length > 1){
+
+		if (arrayToBeOrdered.length > 2) {
+			for (; i < arrayToBeOrdered.length - 1; i += 2) {
+				int aux = arrayToBeOrdered[i];
+				arrayToBeOrdered[i] = arrayToBeOrdered[i + 1];
+				arrayToBeOrdered[i + 1] = aux;
+			}
+		} else if (arrayToBeOrdered.length > 1) {
 			int aux = arrayToBeOrdered[i];
 			arrayToBeOrdered[i] = arrayToBeOrdered[i + 1];
 			arrayToBeOrdered[i + 1] = aux;
 		}
-		
+
 		return arrayToBeOrdered;
 	}
 }
